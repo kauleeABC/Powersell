366c085c37079164fb2eba97f241e701c5e53ddc
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-03-26 11:27:13 -0500

    Add files via upload

---
+++ Update_Services_Account.ps1
@@ -0,0 +1,43 @@
+﻿<#
+author: Kau Lee
+Date: 3/26/2021
+
+This script is to change services account and password
+this can be automate with foreach loop for multiple servers or services
+
+#>
+
+<#
+Clear-Host
+
+Get-WmiObject -Class Win32_service |
+Get-Member -MemberType Method
+#>
+
+<#
+These are .Change parameter
+$DisplayName,
+$PathName,
+$ServiceType,
+$ErrorControl,
+$StartMode,
+$DesktopInteract,
+$StartName,
+$StartPassword,
+$LoadOrderGroup,
+$LoadOrderGroupDependencies,
+$ServiceDependencies
+
+#>
+
+$server = 'server name'
+$user='AccountName'
+$password= 'Password'
+
+##Change log on as account and password
+
+$Srv = Get-WmiObject Win32_service -computer $server -filter "name='JAMSAgent'"
+$Srv.Change($null,$null,$null,$null,$null,$null,$user,$password)
+
+# Change to Local System
+# $LocalSrv.Change($null,$null,$null,$null,$null,$null,"LocalSystem",$null)
