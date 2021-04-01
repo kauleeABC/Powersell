698cab57bb44002c90e253a188771af079348210
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-04-01 12:59:39 -0500

    Add files via upload

---
+++ CheckModule_If_Installed.ps1
@@ -0,0 +1,12 @@
+﻿<#
+Author: Kau Lee
+Date:4/1/2021
+Note: Replace sqlps with the module you want to search
+#>
+#Check from list of module
+if (Get-Module -ListAvailable -Name sqlps) {
+    Write-Host "Module exists"
+} 
+else {
+    Write-Host "Module does not exist"
+}
