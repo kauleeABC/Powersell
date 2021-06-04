fdc2553d823061eb1487728bf10bda2201bbd41c
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-06-04 12:13:27 -0500

    Add files via upload

---
+++ getSRV_Version.ps1
@@ -0,0 +1,7 @@
+﻿#GET Server OS version
+$SRV = Get-Content -Path "C:\MSSQL\SQL_SRV_Master\SQLSRVMasterList.csv"
+
+Get-CimInstance Win32_OperatingSystem -ComputerName $SRV | Select-Object CSName, Caption, Version | #, ServicePackMajorVersion, OSArchitecture,  WindowsDirectory |
+Export-Csv -Path "C:\MSSQL\SQL_SRV_Master\SQLSRVMasterList_OS.csv"
+
+#invoke-sqlcmd -serverInstance "T79TDW171SDB013" -query "SELECT @@VERSION, @@ServerName"
