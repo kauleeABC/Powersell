bc5b9c4d730a21d4e0875580e9ca62340db37cf4
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-06-04 12:14:01 -0500

    Update getSRV_Version.ps1

--- getSRV_Version.ps1
+++ getSRV_Version.ps1
@@ -4,4 +4,4 @@ $SRV = Get-Content -Path "C:\MSSQL\SQL_SRV_Master\SQLSRVMasterList.csv"
 Get-CimInstance Win32_OperatingSystem -ComputerName $SRV | Select-Object CSName, Caption, Version | #, ServicePackMajorVersion, OSArchitecture,  WindowsDirectory |
 Export-Csv -Path "C:\MSSQL\SQL_SRV_Master\SQLSRVMasterList_OS.csv"
 
-#invoke-sqlcmd -serverInstance "T79TDW171SDB013" -query "SELECT @@VERSION, @@ServerName"
+#invoke-sqlcmd -serverInstance $SRV -query "SELECT @@VERSION, @@ServerName"
