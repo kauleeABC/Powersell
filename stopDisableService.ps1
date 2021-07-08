28ec2103938b3fad16975ddfdd9c8f5d12cb5d10
kauleeABC <81389614+kauleeABC@users.noreply.github.com>
2021-07-08 09:28:05 -0500

    Add files via upload

---
+++ stopDisableService.ps1
@@ -0,0 +1,23 @@
+﻿<#
+created by:Kau Lee
+Date: 7/7/2021
+
+this script is to stop and disable service
+
+#>
+
+
+
+#enter server or list of servers, example: 'TDW171SDB001','TDW171SDB002','TDW171SDB005'
+$s= #list of servers
+#service name
+$service='print spooler'
+
+#stop and disable startup
+Get-Service $service -ComputerName $s | Stop-Service -PassThru | Set-Service -StartupType disabled
+
+#checking 
+Get-Service $service -ComputerName $s | Select-Object -Property Machinename, Name, StartType, Status
+
+
+
